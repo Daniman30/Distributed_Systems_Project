40a55d5e659ea0f3fbe95b0b6efb95161e5f70e3
MaferGlez03 <mariafernandasuarezglez@gmail.com>
2024-11-20 23:44:54 -0500

    authentication first version

---
+++ schedule/views.py
@@ -0,0 +1,25 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+@api_view(['POST'])
+def log_in (request):
+    return Response({})
+
+@api_view(['POST'])
+def sign_up (request):
+    serializer = UserSerializer(data= request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user.set_password(request.data['password'])
+        user.save()
+        user = User.objects.get(username = request.data['username'])
+        token =Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data})
+
+@api_view(['GET'])
+def test_token (request):
+    return Response({})
