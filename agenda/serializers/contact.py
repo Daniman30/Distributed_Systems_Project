ee673b11636f770fe14497df794c563ae50f27ea
MaferGlez03 <mariafernandasuarezglez@gmail.com>
2024-11-30 23:05:33 -0500

    list contacs fixed

--- agenda/serializers/contact.py
+++ agenda/serializers/contact.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from agenda.models import Contact
 
 class ContactSerializer(serializers.ModelSerializer):
+    contact_name = serializers.CharField(source='contact.username', read_only=True)
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = ['id', 'owner', 'contact','contact_name', 'created_at'] 
